59059cc107370f7eac18fa19a2e902ab8c7bf2c8
Abdulla Shaker <abdullashaker002@gmail.com>
2025-08-25 00:38:36 +0300

    all answers

--- controlled-forms-in-react-lab/src/App.jsx
+++ controlled-forms-in-react-lab/src/App.jsx
@@ -1,14 +1,58 @@
-
 import './App.css';
 import Bookshelf from './Bookshelf.jsx';
+import { useState } from "react";
 
-const App = () => {
+function Bookshelf() {
+  const [books, setBooks] = useState([
+    { title: "Fourth Wing", author: "Rebecca Yarros" },
+    { title: "The Lion, the Witch and the Wardrobe", author: "C.S. Lewis" }
+  ]);
+
+  const [newBook, setNewBook] = useState({ title: "", author: "" });
+
+  // when we type in the box it changes 
+  function handleInputChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    setNewBook({ ...newBook, [name]: value });
+  }
+  // this fn runs on form submit
+  function handleSubmit(e) {
+    e.preventDefault();
+    setBooks([...books, newBook]);
+    setNewBook({ title: "", author: "" });
+  }
+
   return (
-    <>
-      <h1>My Bookshelf</h1>
-      <Bookshelf />
-    </>
-  );
-};
+    <div>
+      <h3>Add a Book</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="title"
+          value={newBook.title}
+          onChange={handleInputChange}
+          placeholder="Title"
+        />
+        <input
+          name="author"
+          value={newBook.author}
+          onChange={handleInputChange}
+          placeholder="Author"
+        />
+        <button>Add</button>
+      </form>
 
-export default App;
+      {books.map((b, i) => (
+        <div key={i}>
+          <h4>{b.title}</h4>
+          <p>{b.author}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Bookshelf;
+>>>>>>> ade636d (final push)
+
+
